e7599e3d215ed74ae57ede7cddca9832cd054b13
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-07-29 14:47:52 +0100

    Minimum implementation to begin with

--- slack-lambda.js
+++ slack-lambda.js
@@ -1,37 +1,16 @@
-const https = require('https');
-const qs = require('querystring');
-const VERIFICATION_TOKEN = process.env.ENV_VARIABLE.VERIFICATION_TOKEN,
-const ACCESS_TOKEN = process.env.ENV_VARIABLE.ACCESS_TOKEN;
-
-// Verify Url - https://api.slack.com/events/url_verification
-function verify(data, callback) {
-    if (data.token === VERIFICATION_TOKEN) callback(null, data.challenge);
-    else callback("verification failed");   
-}
-
-// Post message to Slack - https://api.slack.com/methods/chat.postMessage
-function process(event, callback) {
-    // test the message for a match and not a bot
-    if (!event.bot_id && /(aws|lambda)/ig.test(event.text)) {
-        var text = `<@${event.user}> Send from process`;
-        var message = { 
-            token: ACCESS_TOKEN,
-            channel: event.channel,
-            text: text
-        };
-
-        var query = qs.stringify(message); // prepare the querystring
-        https.get(`https://slack.com/api/chat.postMessage?${query}`);
-    }
-
-    callback(null);
-}
-
 // Lambda handler
 exports.handler = (data, context, callback) => {
-    switch (data.type) {
-        case "url_verification": verify(data, callback); break;
-        case "event_callback": process(data.event, callback); break;
-        default: callback(null);
+    console.log(data);
+    if(!data.body)
+    {
+        data = {
+            body: {
+                "type": "url_verification",
+    	        "token": "0XM4M4LJ5ZUfxX6PghXqSVaq",
+    	        "challenge": "YwcpXAfHE0d1OXcTMQNa9Ox7YFxkYgWFU4idhaxHARep8vUJLg1F"
+            }
+        };
     }
+    
+    callback(null, {"statusCode": 200, "body": data.body});
 };
