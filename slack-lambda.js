a1a955a4914586995dfc1c0dd34b6cb4666b9e61
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-07-29 16:21:13 +0100

    This is able to validate verificaton and sends a message to the slack channel when api is hit.

--- slack-lambda.js
+++ slack-lambda.js
@@ -1,16 +1,56 @@
+const https = require('https');
+const qs = require('querystring');
+const VERIFICATION_TOKEN = process.env.VERIFICATION_TOKEN;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+
+
+doCallback = (statusCode, body, callback) => {
+    callback(null, {"statusCode": statusCode, "body": body});
+};
+
+validateVerificationToken = (body, callback) => {
+    if (body.token === VERIFICATION_TOKEN){
+        doCallback(200, body.challenge, callback);
+        return;
+    }
+    doCallback(402, "verification failed", callback);   
+};
+
+processEvent = (event, callback) =>{
+
+        var text = `<@${event.user}> Send from process`;
+        var message = { 
+            token: ACCESS_TOKEN,
+            channel: event.channel,
+            text: text
+        };
+
+        var query = qs.stringify(message); // prepare the querystring
+        https.get(`https://slack.com/api/chat.postMessage?${query}`);
+
+    //doCallback(200, event + "55", callback);
+    doCallback(200, null, callback);
+};
+
 // Lambda handler
 exports.handler = (data, context, callback) => {
-    console.log(data);
-    if(!data.body)
+    var body = JSON.parse(data.body);
+    try
     {
-        data = {
-            body: {
-                "type": "url_verification",
-    	        "token": "0XM4M4LJ5ZUfxX6PghXqSVaq",
-    	        "challenge": "YwcpXAfHE0d1OXcTMQNa9Ox7YFxkYgWFU4idhaxHARep8vUJLg1F"
-            }
-        };
+        switch(body.type)
+        {
+            case "url_verification": 
+                validateVerificationToken(body, callback);
+            break;
+            case "event_callback": 
+                processEvent(body.event, callback);
+            break;
+            default: 
+                doCallback(404, "Invalid type received: " + body.type, callback);
+        }
+    }catch(e)
+    {
+        doCallback(500, e, callback);
     }
-    
-    callback(null, {"statusCode": 200, "body": data.body});
+    //doCallback(200, body.token, callback);
 };
